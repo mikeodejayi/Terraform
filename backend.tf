0366667d0aac341ac6850ab80ff1fb3d80b9c66f
Femi Ogunsakin <peace4eva74@gmail.com>
2023-03-04 03:31:08 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Terra-rg-test"
-    storage_account_name = "terrastorage22"
-    container_name       = "terra2356"
+    resource_group_name  = "terratest-rg"
+    storage_account_name = "terrargtest"
+    container_name       = "terracontainer"
     key                  = "terraform.tfstate"
   }
 }
