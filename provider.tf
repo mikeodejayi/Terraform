057a0e969698e5fd209f6e428525361d747dd569
Femi Ogunsakin <peace4eva74@gmail.com>
2023-03-04 04:03:33 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "=2.46.0"
   features {}
 }
