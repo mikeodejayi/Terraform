ca44ca22b4b58c650b68cbe19db40ffdaf20cfcf
Femi Ogunsakin <peace4eva74@gmail.com>
2023-03-04 04:07:52 +0000

    Updated create-app-svc.tf

--- create-app-svc.tf
+++ create-app-svc.tf
@@ -1,34 +1,19 @@
-variable "resource_group_name" {
-  type    = string
-  default = "dev-environment-rg"
-}
-
-variable "location" {
-  type    = string
-  default = "eastus"
-}
-
-variable "app_service_plan_sku" {
-  type    = string
-  default = "Standard_S1"
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "dev-rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "dev-environment-rg"
+  location = "east US"
 }
 
 # Create app service plan
 resource "azurerm_app_service_plan" "service-plan" {
-  name                = "simple-service-plan"
-  location            = azurerm_resource_group.dev-rg.location
+  name = "simple-service-plan45"
+  location = azurerm_resource_group.dev-rg.location
   resource_group_name = azurerm_resource_group.dev-rg.name
-  kind                = "Linux"
-  reserved            = true
+  kind = "Linux"
+  reserved = true
   sku {
     tier = "Standard"
-    size = var.app_service_plan_sku
+    size = "S1"
   }
   tags = {
     environment = "dev"
@@ -37,16 +22,15 @@ resource "azurerm_app_service_plan" "service-plan" {
 
 # Create JAVA app service
 resource "azurerm_app_service" "app-service" {
-  name                = "my-awesome-app-svc"
-  location            = azurerm_resource_group.dev-rg.location
+  name = "my-awesome-app-svc65"
+  location = azurerm_resource_group.dev-rg.location
   resource_group_name = azurerm_resource_group.dev-rg.name
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
-  site_config {
+site_config {
     linux_fx_version = "TOMCAT|8.5-java11"
   }
-
-  tags = {
+tags = {
     environment = "dev"
   }
 }
